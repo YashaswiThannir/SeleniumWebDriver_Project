8bdd626b73ed3158831ddfd25356df2d212484a8
Yashaswi Thannir <YashaswiThannir@users.noreply.github.com>
2021-02-24 23:37:26 -0800

    Create EntryPointIDSelector.cs

---
+++ EntryPointIDSelector.cs
@@ -0,0 +1,46 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+class EntryPointIDSelector
+{
+    static void Main()
+    {
+        string url = "http://testing.todorvachev.com/selectors/id/";
+
+        string ID = "testImage";
+
+        IWebDriver driver = new ChromeDriver();
+
+        driver.Navigate().GoToUrl(url);
+
+        IWebElement element = driver.FindElement(By.Id(ID));
+
+        if(element.Displayed)
+        {
+            GreenMessage("Yes, the element is dispalyed");
+        }
+        else
+        {
+            RedMessage("Element couldn't be found");
+        }
+
+
+    
+    }
+
+    private static void RedMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    private static void GreenMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
+
